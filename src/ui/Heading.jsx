44fdad63059c67a90485c913d240f78fde495e0e
Yohannes Mesay <johnnymesay@gmail.com>
2024-07-05 06:08:32 +0300

    update

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,11 +1,33 @@
-import styled ,{css} from "styled-components";
+import styled, { css } from "styled-components";
 
-const newer = css`
-  color: navajowhite;
-`;
+// const test = css`
+//   text-align: center;
+//   ${10 > 5 && "background-color: yellow"}
+// `;
+
 const Heading = styled.h1`
-  background-color: #67afed;
-  ${newer}
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+    
+    ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+    
+  line-height: 1.4;
 `;
 
 export default Heading;
