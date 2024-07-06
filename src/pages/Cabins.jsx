e966ab6056d5d0bcbe7e4baa08b9333175942bd8
Yohannes Mesay <johnnymesay@gmail.com>
2024-07-06 15:10:58 +0300

    chore: Update npm dependencies for React Query to version 4.36.1

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,16 +1,18 @@
 import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import getCabins from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
